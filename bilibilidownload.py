b1eb6c5ccb2099d86342712597a99d3a3b2e5eea
hsz <249808574@qq.com>
2019-11-11 19:20:21 +0800

    download

--- bilibilidownload.py
+++ bilibilidownload.py
@@ -2,6 +2,17 @@ import sys
 from you_get import common as you_get       #导入you-get库
 
 directory = r'fun/'                         #设置下载目录
-url = 'https://www.bilibili.com/video/av74617372/'      #需要下载的视频地址
-sys.argv = ['you-get','-o',directory,url]       #sys传递参数执行下载，就像在命令行一样
-you_get.main()
+base_url = 'https://www.bilibili.com/video/av'
+aid_file = open("xiaomoaid_1.txt", "r")
+aid_data = aid_file.read()
+all_aid = aid_data.split('\n')
+
+for i in range(3):
+# for aid in all_aid:
+    aid = all_aid[i]
+    print(str(aid))
+    url = base_url + str(aid)
+    sys.argv = ['you-get','-o',directory,url]       #sys传递参数执行下载，就像在命令行一样
+    you_get.main()
+
+aid_file.close()
